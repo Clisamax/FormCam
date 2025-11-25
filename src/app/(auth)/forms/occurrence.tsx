98b6c6b3c78d6a39f58fbeebb9ea22323b6a81aa
Clisamax <99000834+Clisamax@users.noreply.github.com>
2025-11-24 21:03:51 -0300

    chore: configurações dos emuladores e no occurrence.tsx TouchableWithoutFeedback

--- src/app/(auth)/forms/occurrence.tsx
+++ src/app/(auth)/forms/occurrence.tsx
@@ -1,7 +1,13 @@
 import { router } from 'expo-router';
 import { useContext, useRef } from 'react';
 import { Control, useFormContext } from 'react-hook-form';
-import { Text, TextInput, View } from 'react-native';
+import {
+	Keyboard,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
+} from 'react-native';
 
 import Button from '@/components/button/button';
 import Progress from '@/components/progress/progress';
@@ -32,93 +38,98 @@ const Occurrence: React.FC<homeFormData> = (data) => {
 	const sapRef = useRef<TextInput>(null);
 
 	return (
-		<View style={styles.container}>
-			<View style={[styles.containerTop]}>
-				<View style={[styles.containerTopLefth, { padding: 10 }]}>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+			<View style={styles.container}>
+				<View style={[styles.containerTop]}>
+					<View style={[styles.containerTopLefth, { padding: 10 }]}>
+						<Button
+							iconName="back"
+							title="retornar"
+							onPress={() => router.back()}
+							variant="outline"
+							styles={{ height: 50, width: 100 }}
+							fontSize={12}
+							size={14}
+						/>
+					</View>
+					<View style={styles.containerTopRight}>
+						<Text>
+							<Text style={styles.topTextName}>
+								<Text style={styles.text}>Olá</Text> {` ${user?.name}`}{' '}
+								<Text style={{ color: COLORS.gray[400] }}>
+									{' '}
+									{`${user?.sap}`}
+								</Text>
+							</Text>
+						</Text>
+						<Progress progress={100} />
+						<Text style={[styles.textSpace, { color: COLORS.red[500] }]}>
+							<Text
+								style={{
+									color: COLORS.gray[600],
+									fontFamily: FONTES.FONTS.defaultBold,
+								}}
+							>
+								UUID {''}
+							</Text>
+							{`${uuid}`}
+						</Text>
+					</View>
+				</View>
+				<View style={styles.containerMid}>
+					<View>
+						<Text style={{ fontFamily: FONTES.FONTS.code }}>
+							<Text
+								style={{
+									color: COLORS.red[600],
+									fontFamily: FONTES.FONTS.defaultBold,
+								}}
+							>
+								Origem :{' '}
+							</Text>
+							{`${origin} => ${process}`}
+						</Text>
+					</View>
+					<Text style={[styles.text, { textAlign: 'center' }]}>
+						Selecione a opção desejada para continuar
+					</Text>
+				</View>
+				<View style={styles.containerFoot}>
+					<RadioField
+						name="options_5"
+						control={control as unknown as Control}
+						options={[
+							{ label: 'Depósito', value: 'Depósito' },
+							{ label: 'Transportadora', value: 'Transportadora' },
+							{ label: 'Fábrica', value: 'Fábrica' },
+						]}
+						//label="Selecione a area de avaria"
+						rules={{ required: 'Este campo é obrigatório' }}
+					/>
+					<Input
+						error={''}
+						formProps={{
+							name: 'annotation',
+							control: control as unknown as Control,
+							rules: undefined,
+						}}
+						inputProps={{
+							placeholder: 'occurrence details',
+							autoCapitalize: 'none',
+							onSubmitEditing: () => sapRef.current?.focus(),
+						}}
+					/>
 					<Button
-						iconName="back"
-						title="retornar"
-						onPress={() => router.back()}
-						variant="outline"
-						styles={{ height: 50, width: 100 }}
-						fontSize={12}
-						size={14}
+						title={'enviar'}
+						onPress={() => {
+							router.navigate('/(auth)/forms/review');
+							console.log('occurrence:', getValues('options_5'));
+							console.log('annotation:', getValues('annotation'));
+						}}
 					/>
 				</View>
-				<View style={styles.containerTopRight}>
-					<Text>
-						<Text style={styles.topTextName}>
-							<Text style={styles.text}>Olá</Text> {` ${user?.name}`}{' '}
-							<Text style={{ color: COLORS.gray[400] }}> {`${user?.sap}`}</Text>
-						</Text>
-					</Text>
-					<Progress progress={100} />
-					<Text style={[styles.textSpace, { color: COLORS.red[500] }]}>
-						<Text
-							style={{
-								color: COLORS.gray[600],
-								fontFamily: FONTES.FONTS.defaultBold,
-							}}
-						>
-							UUID {''}
-						</Text>
-						{`${uuid}`}
-					</Text>
-				</View>
 			</View>
-			<View style={styles.containerMid}>
-				<View>
-					<Text style={{ fontFamily: FONTES.FONTS.code }}>
-						<Text
-							style={{
-								color: COLORS.red[600],
-								fontFamily: FONTES.FONTS.defaultBold,
-							}}
-						>
-							Origem :{' '}
-						</Text>
-						{`${origin} => ${process}`}
-					</Text>
-				</View>
-				<Text style={[styles.text, { textAlign: 'center' }]}>
-					Selecione a opção desejada para continuar
-				</Text>
-			</View>
-			<View style={styles.containerFoot}>
-				<RadioField
-					name="options_5"
-					control={control as unknown as Control}
-					options={[
-						{ label: 'Depósito', value: 'Depósito' },
-						{ label: 'Transportadora', value: 'Transportadora' },
-						{ label: 'Fábrica', value: 'Fábrica' },
-					]}
-					//label="Selecione a area de avaria"
-					rules={{ required: 'Este campo é obrigatório' }}
-				/>
-				<Input
-					error={''}
-					formProps={{
-						name: 'annotation',
-						control: control as unknown as Control,
-						rules: undefined,
-					}}
-					inputProps={{
-						placeholder: 'occurrence details',
-						autoCapitalize: 'none',
-						onSubmitEditing: () => sapRef.current?.focus(),
-					}}
-				/>
-				<Button
-					title={'enviar'}
-					onPress={() => {
-						router.navigate('/(auth)/forms/review');
-						console.log('occurrence:', getValues('options_5'));
-						console.log('annotation:', getValues('annotation'));
-					}}
-				/>
-			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 };
 export default Occurrence;
